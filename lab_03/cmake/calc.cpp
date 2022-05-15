3c9b10d0b29af56532c1b070a690868e188f9b08
vsevolodkleshchenko <k.vd@icloud.com>
2022-05-15 13:20:05 +0300

    calc.cpp & hanoy.cpp

---
+++ lab_03/cmake/calc.cpp
@@ -0,0 +1,39 @@
+﻿// calc.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+double pow(double operand1, double operand2)
+{
+    double result = 1.0;
+    for (int i = 1; i <= operand2; i++)
+        result *= operand1;
+    return result;
+}
+
+double calculator(double operand1, char operator1, double operand2)
+{
+    switch (operator1)
+    {
+        case '+':
+            return operand1 + operand2;
+        case '-':
+            return operand1 - operand2;
+        case '^':
+            return pow(operand1, operand2);
+        default:
+            break;
+    }
+}
+
+
+int main()
+{
+    int operand1, operand2;
+    char operator1;
+
+    std::cout << "Format: a+b | a-b | a^b" << std::endl;
+    std::cin >> operand1 >> operator1 >> operand2;
+    std::cout << calculator(operand1, operator1, operand2);
+}
+
---
+++ lab_03/cmake/hanoy.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+using namespace std;
+
+
+template <class T>
+struct Elem
+{
+    T data;
+    Elem* next;
+    Elem* prev;
+};
+
+
+template <class T>
+class List
+{
+    Elem<T>* begin;
+    Elem<T>* end;
+    int count;
+
+public:
+    List()
+    {
+        begin = end = nullptr;
+        count = 0;
+    }
+
+    void append(T newdata)
+    {
+        Elem<T>* elem = new Elem<T>;
+        elem->next = nullptr;
+        elem->data = newdata;
+        elem->prev = end;
+        if (end == nullptr)
+            begin = end = elem;
+        else
+        {
+            end->next = elem;
+            end = elem;
+        }
+        count++;
+    }
+
+    void print()
+    {
+        if (count != 0)
+        {
+            Elem<T>* curr = begin;
+            cout << "[ ";
+            while (curr != nullptr)
+            {
+                cout << curr->data << " ";
+                curr = curr->next;
+            }
+            cout << " ]";
+        }
+    }
+
+    void clear()
+    {
+        while (begin != nullptr)
+        {
+            Elem<T>* del = begin;
+            begin = begin->next;
+            delete del;
+        }
+
+    }
+
+    //~List()
+    //{
+    //    clear();
+    //}
+};
+
+
+List<char> hanoi(int n, char s, char f, char h, List<char> L)
+{
+    if (n == 0)
+        return L;
+    L = hanoi(n - 1, s, h, f, L);
+    L.append(s);
+    L.append('-');
+    L.append(f);
+    L.append(' ');
+    L = hanoi(n - 1, h, f, s, L);
+    return L;
+}
+
+
+int main()
+{
+    int n = 6;
+    List<char> L;
+    L = hanoi(n, 's', 'f', 'h', L);
+    L.print();
+    L.clear();
+    L.print();
+}
+
